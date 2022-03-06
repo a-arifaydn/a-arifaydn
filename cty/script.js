deb76a8c184a0b360f324c12b2b84a97c40ef808
a-arifaydn <101044750+a-arifaydn@users.noreply.github.com>
2022-03-06 14:07:09 +0300

    Add files via upload

---
+++ cty/script.js
@@ -0,0 +1,26 @@
+import $ from "https://cdn.skypack.dev/jquery@3.6.0";
+
+$(document).ready(function () {
+  $("#message").keyup(function () {
+    var maxLength = 120;
+    var length = $(this).val().length;
+
+    $("#message-characters").text(maxLength - length + " characters left.");
+
+    if (length > maxLength) {
+      $("#message-characters").css({
+        color: "#ccc"
+      });
+
+      $("#button-send").attr({
+        disabled: "disabled"
+      });
+    } else {
+      $("#message-characters").css({
+        color: "#fff"
+      });
+
+      $("#button-send").removeAttr("disabled");
+    }
+  });
+});
